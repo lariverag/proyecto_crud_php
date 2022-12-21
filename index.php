aad41e8a92b32870302a21cff46c698127c50261
Alfredo Rivera <70917535+lariverag@users.noreply.github.com>
2022-12-20 22:35:31 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+    require_once("C://xampp/htdocs/proyecto_crud_php/view/head/head.php");
+?>
+
+<a class="btn btn-primary" href="/proyecto_crud_php/view/username/create.php">Agrega nuevo Usuario</a>
+
+
+
+<?php
+    require_once("C://xampp/htdocs/proyecto_crud_php/view/head/footer.php");
+?>
