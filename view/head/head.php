3d01f16c0d08b2886997a023e9c7ab1501172ec4
Alfredo Rivera <lariveragut@unadvirtual.edu.co>
2022-12-30 19:22:57 -0500

    se modifico el codigo en head.php para que en los titulos se viera algo referente a las operaciones

--- view/head/head.php
+++ view/head/head.php
@@ -3,7 +3,11 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title><?php
+        echo (empty($_GET['id'])) 
+        ? ((strpos($_SERVER['REQUEST_URI'],'create')) ? "Agregando Nuevo Usuario " : "Index ")
+        : ((strpos($_SERVER['REQUEST_URI'],'show')) ? "Detalles del Registro ".$_GET['id'] : "Actualizar Registro ".$_GET['id']); 
+    ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   </head>
   <body>
